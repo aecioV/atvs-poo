cf017bdd693ad049360016223142fb746df08905
aecioV <129795938+aecioV@users.noreply.github.com>
2023-04-03 21:30:10 -0300

    Update Main.java

--- atvdds-poo-main/lab04/tarefa01/Main.java
+++ atvdds-poo-main/lab04/tarefa01/Main.java
@@ -2,8 +2,8 @@ package lab04.tarefa01;
 
 public class Main {
     public static void main(String[] args){
-        Pessoa p1 = new Pessoa("12345", "Gabriel Luis");
-        Pessoa p2 = new Pessoa("54321", "Álvares de Azevedo");
+        Pessoa p1 = new Pessoa("12345", "Aecio");
+        Pessoa p2 = new Pessoa("54321", "fulano");
 
         System.out.println("Dados da primeira pessoa: ");
         System.out.println(p1.toString());
