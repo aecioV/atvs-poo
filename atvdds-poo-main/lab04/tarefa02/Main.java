090225c1190fc593f6be57f0dbbf028b0539f163
aecioV <129795938+aecioV@users.noreply.github.com>
2023-04-03 21:34:18 -0300

    Update Main.java

--- atvdds-poo-main/lab04/tarefa02/Main.java
+++ atvdds-poo-main/lab04/tarefa02/Main.java
@@ -2,8 +2,8 @@ package lab04.tarefa02;
 
 public class Main {
     public static void main(String[] args) {
-        Livro livro1 = new Livro("Navio negreiro", "Castro Alves", "sla");
-        Livro livro2 = new Livro("Iracema", "José de alencar", "sla", "desrição");
+        Livro livro1 = new Livro("Bilhões e bilhões", "Carl Sagan", "descrição");
+        Livro livro2 = new Livro("O mundo assombrado pelos demonios", "Carl Sagan", "a", "desrição");
 
         System.out.println("Livro 1: ");
         System.out.println(livro1.toString());
