d0ad4288fb68fa9ab3088857e848ed2f789ecf99
aecioV <129795938+aecioV@users.noreply.github.com>
2023-04-17 19:56:41 -0300

    Add files via upload

---
+++ atvdds-poo-main/teste/OperadoresLogicos.java
@@ -0,0 +1,28 @@
+package lab03;
+
+public class OperadoresLogicos {
+    public static void main(String[] args) {
+        int i = 0, j = 10;
+        boolean test = false;
+
+        test = (i > 10) && (j++ > 9);
+        System.out.println(i);
+        System.out.println(j);
+        System.out.println(test);
+
+        test = (i > 10) & (j++ > 9);
+        System.out.println(i);
+        System.out.println(j);
+        System.out.println(test);
+
+        test = (i < 10) || (j++ > 9);
+        System.out.println(i);
+        System.out.println(j);
+        System.out.println(test);
+
+        test = (i < 10) | (j++ > 9);
+        System.out.println(i);
+        System.out.println(j);
+        System.out.println(test);
+    }
+}
